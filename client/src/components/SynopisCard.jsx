99e1399c494ea8c615c2c1ebc5cd86ee77ff56bb
Aude Sedillo <aude.sedillo@gmail.com>
2024-04-16 20:29:43 +0200

    ajout du synopis, ajout de l'api credit section acteur

--- client/src/components/SynopisCard.jsx
+++ client/src/components/SynopisCard.jsx
@@ -8,33 +8,25 @@ function SynopisCard() {
     <div>
       <div className="film-presentation">
         <div className="image-container">
-          <p className="image-film">image du film</p>
+          <img
+            className="image-film"
+            src={movies.data.poster_path}
+            alt="Affiche du film"
+          />
         </div>
         <div className="info-film">
           <h1>{movies.data.title}</h1>
           <div className="data-film">
-            <p>Date de sortie</p>
-            <p>Durée du film</p>
-            <p>Genre</p>
+            <p>{movies.data.release_date}</p>
+            <p>{movies.data.runtime} min</p>
+            <p>{movies.data.genres.name}</p>
             <p>Réalisateur</p>
           </div>
         </div>
       </div>
       <div className="synopis">
-        <h2>Résumé</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          asperiores, recusandae reiciendis, eveniet nam iusto, laborum
-          laudantium fugit earum assumenda ad aliquam et. Incidunt, quisquam
-          tempore! Expedita voluptatibus reiciendis quis. Dignissimos saepe
-          quidem quia rerum odit? Reprehenderit reiciendis, facere dicta beatae
-          consequuntur aliquid suscipit temporibus nisi amet culpa, soluta natus
-          nam, rem sed cumque! Velit aliquam quam modi repellendus sunt. Ullam
-          qui doloremque recusandae nesciunt atque officiis accusantium vitae
-          quaerat minus eveniet omnis perspiciatis vel sapiente quae molestias
-          tenetur voluptate, officia deserunt voluptates placeat sunt minima
-          voluptatibus quibusdam! Expedita, deserunt?
-        </p>
+        <h2>Synopis</h2>
+        <p>{movies.data.overview}</p>
       </div>
     </div>
   );
--- client/src/pages/FilmTemplate.jsx
+++ client/src/pages/FilmTemplate.jsx
@@ -1,14 +1,29 @@
+import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import SynopisCard from "../components/SynopisCard";
 
 function FilmTemplate() {
   const movie = useLoaderData();
   console.info(movie);
 
+  const [cast, setCast] = useState();
+  const apiKey = "d18d8616efca4b1c0cfc2fbae4c67c7c";
+
+  axios
+    .get(`https://api.themoviedb.org/3/movie/550/credits?api_key=${apiKey}`)
+    .then((response) => {
+      setCast(response.data);
+      // console.log(response.data);
+    });
+
   return (
     <div>
       <SynopisCard />
-      <div>Film Template</div>
+      <div>
+        <p>Actors</p>
+        <section>{cast?.cast.name}</section>
+      </div>
     </div>
   );
 }
