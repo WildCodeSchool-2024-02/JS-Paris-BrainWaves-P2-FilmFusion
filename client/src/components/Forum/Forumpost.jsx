e83875f96772b173c87b02c208eff35bf46c0a79
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-04-29 09:33:17 +0200

    add forum comment

--- client/src/components/Forum/Forumpost.jsx
+++ client/src/components/Forum/Forumpost.jsx
@@ -1,3 +1,76 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import "./Forumpost.css";
+// import { useNavigate } from "react-router-dom";
+
+// const apiKey = import.meta.env.VITE_APP_API_KEY;
+// const apiUrl = import.meta.env.VITE_APP_API_URL;
+
+// function Forumpost() {
+//   const [movies, setMovies] = useState([]);
+//   const getMovies = () => {
+//     axios
+//       .get(
+//         `
+//         ${apiUrl}/movie/popular?api_key=${apiKey}`
+//       )
+//       .then((response) => {
+//         setMovies(response.data.results);
+//       });
+//   };
+
+//   useEffect(() => {
+//     getMovies();
+//   }, []);
+//   const navigate = useNavigate();
+//   const handleMovieClick = (movieId) => {
+//     navigate(`/forumFilm`, {
+//       state: {
+//         mId: movieId,
+//       },
+//     });
+//   };
+
+//   return (
+//     <div className="post">
+//       {movies.map((movie) => (
+//         <div className="postforum" key={movie.id}>
+//           <h1 className="titlepost">{movie.title}</h1>
+//           <div className="postpost">
+//             <img
+//               className="imageforumpost"
+//               src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+//               alt="poster_movie"
+//             />
+//             <p className="compost">
+//               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
+//               aliquam, libero fugit impedit temporibus eaque alias debitis quas,
+//               eius illo velit, inventore eveniet commodi sapiente dolore
+//               repellat odio soluta ratione.
+//             </p>
+//           </div>
+//           <p className="infopost">
+//             <div className="postcomposent">
+//               <p className="postdate">poster le 00/00/00</p>
+//               <button
+//                 type="button"
+//                 className="postbutton"
+//                 onClick={() => handleMovieClick(movie.id)}
+//                 onKeyDown={() => handleMovieClick()}
+//               >
+//                 {" "}
+//                 button
+//               </button>
+//             </div>
+//           </p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Forumpost;
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./Forumpost.css";
@@ -8,20 +81,31 @@ const apiUrl = import.meta.env.VITE_APP_API_URL;
 
 function Forumpost() {
   const [movies, setMovies] = useState([]);
+  const [comments, setComments] = useState({});
+
   const getMovies = () => {
-    axios
-      .get(
-        `
-        ${apiUrl}/movie/popular?api_key=${apiKey}`
-      )
-      .then((response) => {
-        setMovies(response.data.results);
+    axios.get(`${apiUrl}/discover/movie?api_key=${apiKey}`).then((response) => {
+      setMovies(response.data.results);
+
+      response.data.results.forEach((movie) => {
+        axios
+          .get(`${apiUrl}/movie/${movie.id}/reviews?api_key=${apiKey}`)
+          .then((reviewsResponse) => {
+            if (reviewsResponse.data.results.length > 0) {
+              setComments((prevState) => ({
+                ...prevState,
+                [movie.id]: reviewsResponse.data.results[0],
+              }));
+            }
+          });
       });
+    });
   };
 
   useEffect(() => {
     getMovies();
   }, []);
+
   const navigate = useNavigate();
   const handleMovieClick = (movieId) => {
     navigate(`/forumFilm`, {
@@ -43,22 +127,22 @@ function Forumpost() {
               alt="poster_movie"
             />
             <p className="compost">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
-              aliquam, libero fugit impedit temporibus eaque alias debitis quas,
-              eius illo velit, inventore eveniet commodi sapiente dolore
-              repellat odio soluta ratione.
+              {comments[movie.id] && (
+                <div>
+                  <p>{comments[movie.id].content}</p>
+                </div>
+              )}
             </p>
           </div>
           <p className="infopost">
             <div className="postcomposent">
               <p className="postdate">poster le 00/00/00</p>
+
               <button
                 type="button"
                 className="postbutton"
                 onClick={() => handleMovieClick(movie.id)}
-                onKeyDown={() => handleMovieClick()}
               >
-                {" "}
                 button
               </button>
             </div>
--- client/src/components/Forum/listefilm.jsx
+++ client/src/components/Forum/listefilm.jsx
@@ -1,30 +1,80 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import "./ListeFilm.css";
+
+// const apiKey = import.meta.env.VITE_APP_API_KEY;
+// const apiUrl = import.meta.env.VITE_APP_API_URL;
+
+// function ListeFilm() {
+//   const [movies, setMovies] = useState([]);
+//   const getMovies = () => {
+//     axios
+//       .get(`${apiUrl}/discover/movie?api_key=${apiKey}`)
+//       .then((response) => {
+//         setMovies(response.data.results);
+//       });
+//   };
+
+//   useEffect(() => {
+//     getMovies();
+//   }, []);
+
+//   return (
+//     <div className="listcolor">
+//       <h1 className="listmovies">Liste des fillms</h1>
+//       {movies.map((movie) => (
+//         <div key={movie.title}>
+//           <p className="listfilms">{movie.title}</p>{" "}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default ListeFilm;
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./ListeFilm.css";
+import { useNavigate } from "react-router-dom";
 
 const apiKey = import.meta.env.VITE_APP_API_KEY;
 const apiUrl = import.meta.env.VITE_APP_API_URL;
 
 function ListeFilm() {
   const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
+
   const getMovies = () => {
-    axios
-      .get(`${apiUrl}/discover/movie?api_key=${apiKey}`)
-      .then((response) => {
-        setMovies(response.data.results);
-      });
+    axios.get(`${apiUrl}/discover/movie?api_key=${apiKey}`).then((response) => {
+      setMovies(response.data.results);
+    });
   };
 
   useEffect(() => {
     getMovies();
   }, []);
 
+  const handleMovieClick = (movieId) => {
+    navigate(`/forumFilm`, {
+      state: {
+        mId: movieId,
+      },
+    });
+  };
+
   return (
     <div className="listcolor">
-      <h1 className="listmovies">Liste des fillms</h1>
+      <h1 className="listmovies">Liste des films</h1>
       {movies.map((movie) => (
-        <div key={movie.title}>
-          <p className="listfilms">{movie.title}</p>{" "}
+        <div
+          type="button"
+          role="presentation"
+          key={movie.id}
+          onClick={() => handleMovieClick(movie.id)}
+          onKeyDown={() => handleMovieClick(movie.id)}
+        >
+          <p className="listfilms clickable">{movie.title}</p>
         </div>
       ))}
     </div>
